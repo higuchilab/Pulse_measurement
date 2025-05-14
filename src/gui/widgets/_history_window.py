53f2916e6ebbf82e622470140534a3f12556d73c
Kou-Yamaguchi <kou.tuos.infetc72@gmail.com>
2025-05-14 15:26:41 +0900

    feat: 履歴でグラフを表示させる機能を追加

--- src/gui/widgets/_history_window.py
+++ src/gui/widgets/_history_window.py
@@ -1,20 +1,75 @@
 import tkinter as tk
 from tkinter.ttk import Treeview
-
 from sqlalchemy import select
-# from ...core.data_processing import ReferHistoryParam
-
 from src.database.session_manager import session_scope
-from src.database.models import History
+from src.database.models import (
+    History,
+    TwoTerminalResult,
+    FourTerminalResult,
+    MeasureType,
+)
 from src.gui.widgets import CommonInputForm
+from src.visualization import pulse_graph
 
 class HistoryWindow(tk.Frame):
     def __init__(self, master):
         super().__init__(master=master)
         self.pack(fill="both", expand=True, side="right")
         self.form_top = CommonInputForm(self)
-        tree_view_history = TreeViewHistory(self)
-        tree_view_history.pack()
+        self.tree_view_history = TreeViewHistory(self)
+        self.tree_view_history.pack()
+        self.tree_view_history.bind("<<TreeviewSelect>>", self.on_select)
+
+    def on_select(self, event):
+        """
+        Treeviewの選択イベント
+        選択された行のmeasure_typeから、参照するテーブルを決定、
+        選択された行のhistory_idから、テーブルからデータを取得してグラフを表示する
+        """
+        selected_item = self.tree_view_history.selection()
+        if selected_item:
+            item = self.tree_view_history.item(selected_item)
+            history_id = item["values"][0]
+            measure_type_id = item["values"][1]
+
+            # 3. measure_type_idから参照するテーブルを決定
+            table = self._get_table_by_measure_type(measure_type_id)
+
+            if table:
+                # 4. 参照するテーブルからhistory_idに基づいてデータを取得
+                with session_scope() as session:
+                    stmt = select(table).where(table.history_id == history_id)
+                    data = session.scalars(stmt).all()
+
+                # 5. データをグラフに表示
+                if data:
+                    self._plot_data(data)
+
+    def _get_table_by_measure_type(self, measure_type_id):
+        """
+        measure_type_idに基づいて参照するテーブルを決定
+        """
+        with session_scope() as session:
+            measure_type = session.query(MeasureType).filter_by(id=measure_type_id).first()
+            if measure_type:
+                if measure_type.name == "2-terminal Pulse":
+                    return TwoTerminalResult
+                elif measure_type.name == "4-terminal Pulse":
+                    return FourTerminalResult
+                # 他の測定タイプに応じたテーブルを追加
+        return None
+
+    def _plot_data(self, data):
+        """
+        データをグラフに表示
+        """
+        # データを時系列形式に変換してプロット
+        times = [row.elapsed_time for row in data]
+        voltages = [row.voltage for row in data]
+        currents = [row.current for row in data]
+
+        # グラフを表示 (仮にpulse_graphを使用)
+        pulse_graph(times, voltages, currents)
 
 
 class TreeViewHistory(Treeview):
@@ -30,10 +85,7 @@ class TreeViewHistory(Treeview):
             self.column(col, width=100, minwidth=50)
 
         self.load_data()
-    
-    # def load_history(self, param: ReferHistoryParam):
-    #     for row in refer_history_table(param):
-    #         self.insert("", "end", text="test2", values=row)
+
     def load_data(self):
         """
         データベースから履歴を取得して表示
@@ -44,5 +96,4 @@ class TreeViewHistory(Treeview):
             history_data = session.scalars(stmt).all()
             
             for row in history_data:
-                self.insert("", "end", values=row)
-
+                self.insert("", "end", values=(row.id, row.measure_type_id, row.user_id, row.sample_id, row.option))
